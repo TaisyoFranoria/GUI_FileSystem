1aa1ed55f496d3f566860f5bce79fe3660a004f6
TaisyoFranoria <taisyocbx400fs@gmail.com>
2019-03-02 20:56:12 +0900

    遊び中

--- filesarver/main.cpp
+++ filesarver/main.cpp
@@ -50,16 +50,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	DropFile dr(true);
 
 	std::vector<std::string> path;
+	std::string Fpath;
+
+	//TEst
+	bool once = false;
+	int test_Graph;
+	bool Graph = false;
 	
 
 	while (!ScreenFlip() && !ProcessMessage() && !ClearDrawScreen()) {
-		dr.check();
-		for (int i = 0; i < dr.count; i++) {
-			path.push_back(dr[i]);
-			DrawString(0, i*16, path[i].c_str(), 0xFFFFFF);
+		if (!once) {
+			dr.check();
+			for (int i = 0; i < dr.count; i++) {
+				path.push_back(dr[i]);
+				//DrawString(0, i*16, path[i].c_str(), 0xFFFFFF);
+				Fpath += path[i];
+				test_Graph = LoadGraph(Fpath.c_str());
+				if (!test_Graph == -1) Graph = true;
+				once = true;
+			}
+			
 		}
-
-		
+		DrawString(0,0, Fpath.c_str(), 0xFFFFFF);
+		if (Graph) {
+			DrawGraph(0,16,test_Graph,TRUE);
+			DrawString(0,16,"LOADED!!",0xFFFFFF);
+		}
 
 		if (CheckHitKey(KEY_INPUT_ESCAPE))break;
 	}
